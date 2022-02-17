823b7b6e29beb3ed38120fae47869bbbd8dea6d8
XMoiX22 <moisesjimez1234@gmail.com>
2022-02-17 12:22:22 -0500

    Commit 2

--- tareas.cpp
+++ tareas.cpp
@@ -1,14 +1,53 @@
 #include "tareas.h"
 #include "ui_tareas.h"
 
-tareas::tareas(QWidget *parent) :
+Tareas::Tareas(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::tareas)
+    ui(new Ui::Tareas)
 {
     ui->setupUi(this);
+    this->finalizada = false;
 }
 
-tareas::~tareas()
+Tareas::~Tareas()
 {
     delete ui;
+}
+
+void Tareas::setNumero(int numero){
+    this->numero = numero;
+}
+void Tareas::setNombre(QString nombre){
+    this->nombre = nombre;
+}
+void Tareas::setFecha(QDate fecha)
+{
+    this->fecha = fecha;
+}
+void Tareas::setPrioridad(char prioridad){
+    this->prioridad = prioridad;
+}
+void Tareas::setTipo(QString tipo){
+    this->tipo = tipo;
+}
+void Tareas::finalizarTarea(){
+    this->finalizada = true;
+}
+bool Tareas::isTareaFinalizada(){
+    return this->finalizada;
+}
+int Tareas::getNumero(){
+    return this->numero;
+}
+QString Tareas::getNombre(){
+    return this->nombre;
+}
+QDate Tareas::getFecha(){
+    return this->fecha;
+}
+char Tareas::getPrioridad(){
+    return this->prioridad;
+}
+QString Tareas::getTipo(){
+    return this->tipo;
 }
--- tareas.h
+++ tareas.h
@@ -2,21 +2,43 @@
 #define TAREAS_H
 
 #include <QWidget>
+#include <QDate>
 
 namespace Ui {
-class tareas;
+class Tareas;
 }
 
-class tareas : public QWidget
+class Tareas : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit tareas(QWidget *parent = nullptr);
-    ~tareas();
+    explicit Tareas(QWidget *parent = nullptr);
+    ~Tareas();
+    //lista de mtodos get y set para acceder a las propiedades de las tareas
+    void setNumero(int numero);
+    void setNombre(QString nombre);
+    void setFecha(QDate fecha);
+    void setPrioridad(char prioridad);
+    void setTipo(QString tipo);
+    void finalizarTarea();
+    bool isTareaFinalizada();
+    int getNumero();
+    QString getNombre();
+    QDate getFecha();
+    char getPrioridad();
+    QString getTipo();
+
 
 private:
-    Ui::tareas *ui;
+    Ui::Tareas *ui;
+    //propiedades de las tareas(atributos de la clase)
+    int numero;
+    QString nombre;
+    QDate fecha;
+    char prioridad;
+    QString tipo;
+    bool finalizada;
 };
 
 #endif // TAREAS_H
